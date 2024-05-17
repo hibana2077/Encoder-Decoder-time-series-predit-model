a016ff6ddb0a750f926e6cf920a124cf40d55cb9
hibana2077 <hibana2077@gmail.com>
2024-05-17 11:20:07 +0800

    chore: Update requirements.txt to include matplotlib library

--- data/make_and_label.py
+++ data/make_and_label.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmaill.com
 Date: 2024-05-16 20:52:55
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-05-16 23:12:29
+LastEditTime: 2024-05-16 23:57:38
 FilePath: /Encoder-Decoder-time-series-predit-model/data/make_and_label.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -20,8 +20,8 @@ filterwarnings('ignore')
 args = argparse.ArgumentParser()
 args.add_argument('--symbol', type=str, default='BTC/USDT')
 args.add_argument('--timeframe', type=str, default='1h')
-args.add_argument('--from_date', type=str, default='2024-01-01 00:00:00')
-args.add_argument('--to_date', type=str, default='2024-01-31 00:00:00')
+args.add_argument('--from_date', type=str, default='2023-01-01 00:00:00')
+args.add_argument('--to_date', type=str, default='2024-01-01 00:00:00')
 args = args.parse_args()
 
 # Set up logger
@@ -38,15 +38,17 @@ if __name__ == '__main__':
     from_date:str = args.from_date
     to_date:str = args.to_date
     since = exchange.parse8601(from_date)
-    limit = 500
     data = pd.DataFrame()
     while since < exchange.parse8601(to_date):
-        logger.info(f'Fetching data from {exchange.iso8601(since)} to {exchange.iso8601(since + limit * 60 * 1000)}')
-        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since, limit)
+        logger.info(f'Now Since: {exchange.iso8601(since)}')
+        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since)
         data = pd.concat([data, pd.DataFrame(ohlcv)])
-        since = ohlcv[-1][0] + 1
+        since = ohlcv[-1][0]
     data.columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
     data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
+    data = data.drop_duplicates('timestamp')
     data.set_index('timestamp', inplace=True)
     data.to_csv(f'{symbol.replace("/", "_")}_{timeframe}.csv')
+    logger.info(f'Data shape: {data.shape}')
     logger.info(f'Saved data to data/{symbol.replace("/", "_")}_{timeframe}.csv')
+    logger.info("Done!")
